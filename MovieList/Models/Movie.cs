89ec03cf7a59f82061fc53c7c8a9ac52e290b23e
Zyx750 <maciej.trzaskacz@gmail.com>
2024-05-29 17:17:59 +0200

    space in "Release Year"

--- MovieList/Models/Movie.cs
+++ MovieList/Models/Movie.cs
@@ -6,6 +6,7 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Release Year")]
     public int? ReleaseYear { get; set; }
     public string? Genre { get; set; }
 }
